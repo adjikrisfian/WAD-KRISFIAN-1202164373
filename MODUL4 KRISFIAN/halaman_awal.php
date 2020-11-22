246d215786aa5a3e1d4bc41cb01798dd534faf76
adjikrisfian <48038595+adjikrisfian@users.noreply.github.com>
2020-11-22 19:37:55 +0700

    Add files via upload

---
+++ MODUL4 KRISFIAN/halaman_awal.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Halaman Awal</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">WAD Beauty</a>
+  <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Selamat Datang
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="profil.php">profil</a>
+          <form action="logout.php" method="post">
+            <a class="dropdown-item" type="submit" name="logout">Logout</a>
+          </form>
+        </div>
+      </li>
+    </ul>
+</nav>
+
+  <div class="container"> 
+  <div class="row justify-content-center" style="border:1px solid black; text-align: center">
+    <div class="col" >
+      <h1 style="background-color:DodgerBlue;">WAD BEAUTY</h1>
+      <h5 style="background-color:yellow;">menyediakan berbagai macam produk skin care berualitas tinggi</h5>
+    </div>
+  </div>
+  <div class="row justify-content-center"  style="text-align": center">
+    <div class="col" style="border:1px solid black";>
+    <div class="card" style="width: 18rem;">
+  <img src="erha1.jpg" class="card-img-top" alt="kecantikan">
+  <div class="card-body">
+    <p class="card-text">SAge Corrector Night Moisturizer dengan anti-aging system untuk mempercepat proses regenerasi  kulit, mengembalikan keseimbangan sistem pertahanan alami kulit terhadap proses penuaan.</p>
+  </div>
+</div>
+    </div>
+    <div class="col" style="border:1px solid black";>
+    <div class="card" style="width: 18rem;">
+  <img src="OIP.jpg" class="card-img-top" alt="wardah">
+  <div class="card-body">
+    <p class="card-text">Busa pembersih wajah yang creamy dan foamy dengan formula double lightening system dan moisturizer untuk membersihkan kotoran penyumbat pori, sebum, dan sisa make up secara optimal tanpa membuat kulit menjadi kering.</p>
+  </div>
+</div>
+    </div>
+    <div class="col" style="border:1px solid black";>
+    <div class="card" style="width: 18rem;">
+  <img src="kas.jpg" class="card-img-top" alt="sun screen">
+  <div class="card-body">
+    <p class="card-text">Safi Facial Moisturiser with Gamat Extract & Habbatus Sauda, Controls sebum production Restores skin’s natural moisture, Protects skin with nutrients and antibacterial properties.</p>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+
+  
+    
+  
+    
+  
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
